3d4373137c319bbb058ea306bbb9f665b0d7cff4
El1seius <eliseevsi@gmail.com>
2023-06-09 01:49:50 +0300

    Correction

--- script.py
+++ script.py
@@ -1,16 +1,18 @@
+from datetime import datetime, timedelta
 import json
 import requests
 import settings
 
-def go_to_seller_ozon():
+
+def go_to_seller_ozon(date_start, date_finish):
     go_to_url = "https://api-seller.ozon.ru/v3/posting/fbs/list"
     headers = {'Client-Id': settings.CLIENT_ID, 'Api-Key': settings.USER_API, 'Content-Type': 'application/json'}
     params = {
         'dir': 'asc',
         'filter': {
-            'since': '2023-05-01T00:00:00.000Z',
+            'since': date_start,
             'status': 'delivered',
-            'to': '2023-05-08T00:00:00.000Z'
+            'to': date_finish
         },
         'limit': 1000,
         'offset': 0,
@@ -28,9 +30,15 @@ def go_to_seller_ozon():
         return sell_ozon
     except(requests.RequestException, ValueError):
         print('Сетевая ошибка')
-        return False
-    return False
+        return None
+
+
+delta = timedelta(days=8)
+date_start = datetime.now() - delta
+date_start = date_start.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+date_finish = datetime.now()
+date_finish = date_finish.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
 
 if __name__ == "__main__":
-    print(go_to_seller_ozon())
+    print(go_to_seller_ozon(date_start, date_finish))
