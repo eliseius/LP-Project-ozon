938915aa177c6038e956762eefb1a14f998fcc92
Dmitry-ULCH <darmoll@mail.ru>
2023-06-16 00:15:03 +0300

    Bot in progress

--- project_ozon.py
+++ project_ozon.py
@@ -1,6 +1,5 @@
-import ozon_settings, logging, json
-from datetime import datetime
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import constants, dateparser, json, logging, ozon_settings
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
 
 logging.basicConfig(filename='ozon.log',
@@ -10,29 +9,38 @@ logging.basicConfig(filename='ozon.log',
 
 
 def greet_user(update, context):
-    update.message.reply_text('Здравствуйте, вы используете бота для сбора данных о продажах товаров в страны Таможенного союза через Ozon.\nВведите команду /report, месяц и год в формате "мм.гггг" для получения отчёта:')
+    update.message.reply_text('Здравствуйте, вы используете бота для сбора данных о продажах товаров в страны Таможенного союза через Ozon.\nДля получения отчёта введите команду /report')
 
 
-def collecting_sales (update, context):
-    eacu = ["Армения", "Беларусь", "Казахстан", "Киргизия", "Россия"]
+def output_report(update, context):
+    update.message.reply_text('Введите месяц и год')
+    date_input = update.message.text
+    update.message.reply_text(collecting_sales(date_input))
+
+
+def collecting_sales(date):
+    formatted_date_input = dateparser.parse(date)
     month_sales = []
-    date_input = update.message.text.split()[1]
-    formatted_date_input = datetime.strptime(date_input, "%m.%Y")
     with open("sales.json", "r") as sales:
-        sales_list = json.load(sales)
+        sales_list = json.load(sales)    
     for position in sales_list:
-        date_from_json = datetime.strptime(position["date"][:10], "%Y-%m-%d")
-        if date_from_json.year == formatted_date_input.year and date_from_json.month == formatted_date_input.month and position["country"] in eacu:
-            month_sales += position
-        update.message.reply_text(month_sales)
-        # На данный момент выводит пустой список((( Пока не сообразил, где косяк
+        date_import = dateparser.parse(position["date"])
+        if date_import.year == formatted_date_input.year and date_import.month == formatted_date_input.month and position["country"] in constants.CUSTOMS_UNION:
+            month_sales.append(position)
+    return month_sales
+
+
+def command_request(update, context):
+    user_text = update.message.text
+    update.message.reply_text('Данная команда не поддерживается. Введите команду /report')
+
 
 def main():
     mybot = Updater(ozon_settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(CommandHandler('report', collecting_sales))
-    # dp.add_handler(MessageHandler(Filters.text, function))
+    dp.add_handler(CommandHandler('report', output_report))
+    dp.add_handler(MessageHandler(Filters.text, command_request))
     logging.info('BOT STARTED')
     mybot.start_polling()
     mybot.idle()
