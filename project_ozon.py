b9cdf1d546ffe6d914084cf091310471b38546ac
Dmitry-ULCH <darmoll@mail.ru>
2023-06-17 00:30:01 +0300

    Added 'general_report_output' function request

--- project_ozon.py
+++ project_ozon.py
@@ -1,4 +1,4 @@
-import constants, dateparser, json, logging, ozon_settings
+import dateparser, logging, ozon_settings
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
 
@@ -9,37 +9,30 @@ logging.basicConfig(filename='ozon.log',
 
 
 def greet_user(update, context):
-    update.message.reply_text('Здравствуйте, вы используете бота для сбора данных о продажах товаров в страны Таможенного союза через Ozon.\nДля получения отчёта введите команду /report')
+    update.message.reply_text('Здравствуйте, вы используете бота для сбора данных о продажах товаров в страны Таможенного союза через Ozon.\nДля получения отчёта введите команду /report, начало и конец периода в формате "день.месяц.год", разделённые пробелом')
 
 
-def output_report(update, context):
-    update.message.reply_text('Введите месяц и год')
+def take_report(update, context):
     date_input = update.message.text
-    update.message.reply_text(collecting_sales(date_input))
+    date_start = dateparser.parse(date_input.split()[1], settings={'DATE_ORDER': 'DMY'})
+    date_finish = dateparser.parse(date_input.split()[2], settings={'DATE_ORDER': 'DMY'})
+    report_output = general_report_output(date_start, date_finish, limit, offset)
+    update.message.reply_text(report_output)
 
 
-def collecting_sales(date):
-    formatted_date_input = dateparser.parse(date)
-    month_sales = []
-    with open("sales.json", "r") as sales:
-        sales_list = json.load(sales)    
-    for position in sales_list:
-        date_import = dateparser.parse(position["date"])
-        if date_import.year == formatted_date_input.year and date_import.month == formatted_date_input.month and position["country"] in constants.CUSTOMS_UNION:
-            month_sales.append(position)
-    return month_sales
+def general_report_output(date_start, date_finish, limit, offset):
 
 
 def command_request(update, context):
     user_text = update.message.text
-    update.message.reply_text('Данная команда не поддерживается. Введите команду /report')
+    update.message.reply_text('Данная команда не поддерживается. Введите команду /report, начало и конец периода в формате "день.месяц.год", разделённые пробелом')
 
 
 def main():
     mybot = Updater(ozon_settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(CommandHandler('report', output_report))
+    dp.add_handler(CommandHandler('report', take_report))
     dp.add_handler(MessageHandler(Filters.text, command_request))
     logging.info('BOT STARTED')
     mybot.start_polling()
