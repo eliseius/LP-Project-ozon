a2bd456637ca467fd825aa474edada5d13c3cb51
El1seius <eliseevsi@gmail.com>
2023-06-27 23:01:05 +0300

    Remade

--- script.py
+++ script.py
@@ -5,51 +5,64 @@ import os
 import requests
 
 
-def getting_sales_data(date_start, date_finish, limit, offset):
+def get_sales_data(date_start, date_finish):
+    limit = 1000
+    offset = 0
+    report_pagination = []
+
+    while True:
+        sales_report = get_global_report(date_start, date_finish, limit, offset)
+        try:
+            value_pagination = sales_report['result']['has_next']
+        except TypeError:
+            break
+
+        if value_pagination:
+            short_report = make_short_report(sales_report)
+            report_pagination.extend(short_report)
+            offset += limit
+        else:
+            short_report = make_short_report(sales_report)
+            report_pagination.extend(short_report)
+            break
+    
+    return report_pagination
+
+
+def get_global_report(date_start, date_finish, limit, offset):
     str_datetime_start = datetime.strftime(date_start, '%Y-%m-%dT%H:%M:%S.%fZ')
     str_datetime_finish = datetime.strftime(date_finish, '%Y-%m-%dT%H:%M:%S.%fZ')
 
-    report_pagination = []
-    while True:
-        go_to_url = "https://api-seller.ozon.ru/v3/posting/fbs/list"
-        headers = {'Client-Id': os.environ['OZON_CLIENT_ID'], 'Api-Key': os.environ['OZON_API_KEY'], 'Content-Type': 'application/json'}
-        params = {
-            'dir': 'asc',
-            'filter': {
-                'since': str_datetime_start,
-                'status': 'delivered',
-                'to': str_datetime_finish
-            },
-            'limit': limit,
-            'offset': offset,
-            'translit': True,
-            'with': {
-                'analytics_data': True,
-                'financial_data': True,
-            }
+    url = "https://api-seller.ozon.ru/v3/posting/fbs/list"
+    headers = {'Client-Id': os.environ['OZON_CLIENT_ID'], 'Api-Key': os.environ['OZON_API_KEY'], 'Content-Type': 'application/json'}
+    params = {
+        'dir': 'asc',
+        'filter': {
+            'since': str_datetime_start,
+            'status': 'delivered',
+            'to': str_datetime_finish
+        },
+        'limit': limit,
+        'offset': offset,
+        'translit': True,
+        'with': {
+            'analytics_data': True,
+            'financial_data': True,
         }
+    }
 
-        params = json.dumps(params)
-        response = requests.post(go_to_url, headers=headers, data=params)
-        if response:
-            try:
-                sales_report = response.json()
-            except(ValueError):
-                print('Ошибка сформированных данных')
-                return None
-        else:
-            print('Сетевая ошибка')
+    params = json.dumps(params)
+    response = requests.post(url, headers=headers, data=params)
+    if response:
+        try:
+            sales_report = response.json()
+            return sales_report
+        except ValueError:
+            print('Ошибка сформированных данных')
             return None
-
-        value_pagination = sales_report['result']['has_next']
-        if value_pagination:
-            make_pagination(sales_report, report_pagination)
-            offset += limit
-        else:
-            make_pagination(sales_report, report_pagination)
-            break
-
-    return report_pagination
+    else:
+        print('Сетевая ошибка')
+        return None
 
 
 def make_short_report(sales_report):
@@ -68,9 +81,3 @@ def make_short_report(sales_report):
         short_report.append(inform_every_item_sold)
 
     return short_report
-
-
-def make_pagination(sales_report, report_pagination):
-    short_report = make_short_report(sales_report)
-    report_pagination.extend(short_report)
-    return report_pagination
